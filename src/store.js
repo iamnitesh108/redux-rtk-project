8b66ccdeb8c72810cb42b504744d46013eb5fae6
iamnitesh108 <iamnitesh108@gmail.com>
2023-09-24 08:28:06 +0530

    actions creator function implemented

--- src/store.js
+++ src/store.js
@@ -41,6 +41,34 @@ console.log(store.getState());
 
 store.dispatch({
   type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car"}
-})
-console.log(store.getState())
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+
+
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan"};
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+console.log(store.getState());
+
+store.dispatch(requestLoan(1000, "Buy a cheap car"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
